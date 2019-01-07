6e88c485b65744a7855d8f8b95e210b2db06d87c
Nick Paridon <nparidon@frontlineed.com>
2019-01-07 08:14:02 -0500

    Initial Commit

---
+++ PublicSchool/Models/Grades.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PublicSchool.Models
+{
+  public enum Grades
+    {
+      APlus,
+      A,
+      AMinus,
+      BPlus,
+      B,
+      BMinus,
+      CPlus,
+      C,
+      CMinus,
+      DPlus,
+      D,
+      DMinus,
+      F
+    }
+}
